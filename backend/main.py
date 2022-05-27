5d1d4f544564b4f51b480e021a29a46a74701a5f
AlexFeve <alexfn8t@gmail.com>
2022-05-26 22:47:58 -0500

    Main citadel adding feature first implementation

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 from datetime import datetime, timezone
+import copy
 
 from api.api_v1 import api  
 
@@ -28,6 +29,7 @@ class Citadel(BaseModel):
     name : str
     system: Optional[str] = None
     submitter : str
+    type : Optional[str] = None  
     fitting : Optional[Fitting] = None #not sure if this will make an array of lists that are optional lol
 
 origins = [ "http://localhost:3000"]
@@ -54,28 +56,76 @@ CITADEL_TABLE = dynamodb_client.Table(TABLE_NAME) #ideally get the table name th
 def hello_world():
     return {"message": "Hello World"}
 
+#https://stackoverflow.com/questions/6696027/how-to-split-elements-of-a-list
+@app.get("/count")
+def get_count():
+    response = CITADEL_TABLE.get_item(Key={'PK': 'TOTAL', 'SK': 'COUNT'})
+    item = response["Item"]
+    #SK = item[0] #might cast this 0 index list into dict?
+    
+    #just make it PK: TOTAL, SK: COUNT and a number value and be done with it...? Got it working as a dict
+
+    if not item:
+        return JSONResponse(status_code=404, content={"message": "Item Not Found"})
+    return jsonable_encoder(item["NumberCit"])
+
 #on the initial creation make a METADATA SK to log important info, first time citadel submitted, name, location, OWNER?
 @app.post("/citadel")
 def create_citadel(newCitadel: Citadel):
 
     if not newCitadel:
         return JSONResponse(status_code=404, content={"message": "Not Recieved Correctly"})
-    #idea being making this part input the meta data then handing over the info to the update or something to put in the first fitting data
-    newCitadel.date = (datetime.now(timezone.utc).isoformat())
-    newCitadel.SK = "Metadata"
 
-    CITADEL_TABLE.put_item(Item=newCitadel.dict())
+    response = CITADEL_TABLE.get_item(Key={'PK': 'TOTAL', 'SK': 'COUNT'})
+    item = response["Item"]
+    uuidVal = item["NumberCit"]
+
+    newCitadel.PK = "Cit#" + str(uuidVal)
+
+    newUUID = int(uuidVal)
+    newUUID +=1
+
+    CITADEL_TABLE.update_item(
+        Key={'PK': 'TOTAL',
+         'SK': 'COUNT'
+         },
+         UpdateExpression='SET NumberCit = :val',
+         ExpressionAttributeValues={
+             ':val': newUUID
+         }
+    )
+    #immediately increment uuid here?
+
+    newCitadel.date = (datetime.now(timezone.utc).isoformat())
+    
+
+    metaTag = copy.deepcopy(newCitadel)
+    metaTag.SK = "METADATA"
+    metaTag.fitting = None
+    metaTag.type = None
+
+    #Have only metadata with location to clean up location gsi
+    #need a citadel type added to metadata
+    #owner go on update tag section to allow change in ownership?
+    
+    newCitadel.SK = newCitadel.date
+    newCitadel.system = None
+
+    CITADEL_TABLE.put_item(Item=metaTag.dict(exclude_none=True))
+    CITADEL_TABLE.put_item(Item=newCitadel.dict(exclude_none=True))
 
 
 # Does the update of a citadel fit
 #Here use the SK of a DATE to enable reading the newest first
 @app.put("/citadel") 
-def update_citFit(updateFit: Citadel):
+def update_citadel_fit(updateFit: Citadel):
 
     if not updateFit:
         return JSONResponse(status_code=404, content={"message": "Not Recieved Correctly"})
 
-    updateFit.date = (datetime.now(timezone.utc).isoformat()) #ideally gets the current time in UTC converted to ISO format here
+    #updateFit.date = (datetime.now(timezone.utc).isoformat()) Do I want the date here even though it will be the SK too?
+    updateFit.SK = (datetime.now(timezone.utc).isoformat())
+
     #send something in the date value json and it will get set here, can easily change this to accepting the frontend if I want or need to later by commenting above line
 
     CITADEL_TABLE.put_item(Item=updateFit.dict()) #Pydantic cast as dict 
@@ -83,10 +133,10 @@ def update_citFit(updateFit: Citadel):
     
     return updateFit 
 
-@app.get("/users/{user_id}")
-def get_user(user_id):
+@app.get("/citadels/{citadel_id}")
+def get_citadel(citadel_id):
 
-    result = CITADEL_TABLE.get_item(Key={'PK': user_id, 'SK': '523'}) #listed as a string in yml, doing a string here?
+    result = CITADEL_TABLE.get_item(Key={'PK': citadel_id, 'SK': 'METADATA'}) #listed as a string in yml, doing a string here?
     item = result.get('Item')
     if not item:
         return JSONResponse(status_code=404, content={"message": "Item Not Found"})
@@ -110,3 +160,8 @@ def get_location(system):
 
 
 handler = Mangum(app)
+
+#5/19 Allow Updating of a citadel on DisplayCitadel
+# setup UUID CITADEL COUNT, on the get item do a check for if it exsists and if not start one at 1 and go from there
+# Figure out a search method on citadel name? Or just on system name as given above and only do edits on displayCit page
+# Turn the locations GSI endpoint into a basic proof of concept page
